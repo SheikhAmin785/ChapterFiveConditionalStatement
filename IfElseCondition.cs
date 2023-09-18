412bb566ee049943df0bd39d16cd962d7238b29d
SheikhAmin785 <shakil785ks@gmail.com>
2023-09-18 23:31:44 +0600

    Add project files.

---
+++ IfElseCondition.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapterFiveConditionalStatement
+{
+    class IfElseCondition
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input two Numbers");
+
+            Console.WriteLine("Enter first number");
+            int firstnumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int secondnumber = int.Parse(Console.ReadLine());
+
+            int bignumber = firstnumber;
+
+            if (secondnumber > firstnumber)
+            {
+                bignumber = secondnumber;
+            }
+
+            Console.WriteLine("the big number is:{0}", bignumber);
+
+        }
+
+        static void Main()
+        {
+            int x = 2;
+            if (x > 3)
+            {
+                Console.WriteLine("x is greater than three");
+            }
+            else
+            {
+                Console.WriteLine("x is not greater than three");
+            }
+        }
+        static void Main()
+        {
+            int a = 5;
+            int b = 3;
+            if (a == b)
+            {
+                Console.WriteLine("this two number are equal");
+            }
+            else
+            {
+                if (a > b)
+                {
+                    Console.WriteLine("number a is greater");
+                }
+                else
+                {
+                    Console.WriteLine("the number b is greater");
+                }
+            }
+        }
+        static void Switch()  //switch case statement
+        {
+            int number = 6;
+            switch (number)
+            {
+                case 1:
+                case 4:
+                case 6:
+                case 8:
+                case 10:
+                    Console.WriteLine("the number is not prime!");break;
+
+                case 2:
+                case 3:
+                case 5:
+                case 7:
+                    Console.WriteLine("the number is prime!");break;
+
+                default:
+                    Console.WriteLine("unknown number!");break;
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ChapterFiveConditionalStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weight = 700;
+            Console.WriteLine(weight >= 500);
+
+            int a = 5;
+            int b = 7;
+            bool condition = (b > a) && (a + b < a * b);
+            Console.WriteLine(condition);
+
+            bool result = (2 < 3) && (3 < 4);
+
+            bool result = (2 < 3) || (1 == 2);
+        }
+    }
+}
+
+//if if-else
+//switch case
+//comparision operator == != >= <= boolean value true false
+//logical operator and or  = &&,||
